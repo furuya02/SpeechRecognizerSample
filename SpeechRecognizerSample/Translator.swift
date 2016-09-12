e6484c3ade78aa70a05676419d9fef6658a462dc
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-09-12 10:55:18 +0900

    first commit

---
+++ SpeechRecognizerSample/Translator.swift
@@ -0,0 +1,64 @@
+    //
+//  Translator.swift
+//  SpeechRecognizerSample
+//
+//  Created by hirauchi.shinichi on 2016/09/12.
+//  Copyright © 2016年 SAPPOROWORKS. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+class Translator {
+
+    fileprivate var accessToken = ""
+
+    init() {
+
+        // アクセストークン取得
+        let clientId = "TranslationTwitterSample"
+        let clientSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+
+        let parameters:Parameters = [
+            "client_id" : clientId,
+            "client_secret" : clientSecret,
+            "scope" : "http://api.microsofttranslator.com",
+            "grant_type" : "client_credentials"
+        ]
+        let url = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13"
+        Alamofire.request(url, method: .post, parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let json = value as? NSDictionary {
+                    self.accessToken = json["access_token"] as! String
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    func conversion(_ message:String, complate: @escaping (String) -> Void) {
+        let query = message as NSString
+        let encodeString = query.addingPercentEscapes(using: String.Encoding.utf8.rawValue)!
+        let headers = [
+            "Authorization": "Bearer \(accessToken)"
+        ]
+        let url = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text=\(encodeString)&from=ja&to=en"
+        Alamofire.request(url, method: .get, headers: headers).responseString { response in
+            switch response.result {
+            case .success(let value):
+                let str:NSString  = value as NSString
+                var array = str.components(separatedBy: NSCharacterSet(charactersIn: "<>") as CharacterSet)
+                if array.count == 5 {
+                    let result = array[2]
+                    complate(result)
+                }
+                break
+            case .failure(let error):
+                complate(error.localizedDescription)
+            }
+        }
+    }
+}
---
+++ SpeechRecognizerSample/ViewController.swift
@@ -0,0 +1,164 @@
+//
+//  ViewController.swift
+//  SpeechRecognizerSample
+//
+//  Created by hirauchi.shinichi on 2016/09/11.
+//  Copyright © 2016年 SAPPOROWORKS. All rights reserved.
+//
+
+import UIKit
+import Speech
+import AVFoundation
+
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    fileprivate let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
+    fileprivate var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    fileprivate var recognitionTask: SFSpeechRecognitionTask?
+    fileprivate let audioEngine = AVAudioEngine()
+    fileprivate var translator = Translator()
+    fileprivate var talker = AVSpeechSynthesizer()
+
+    enum Mode {
+        case none
+        case recording
+        case translation
+    }
+    fileprivate var mode = Mode.none
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        speechRecognizer.delegate = self
+
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            switch authStatus {
+            case .authorized:
+                self.button.isEnabled = true
+            case .denied: // 音声認識へのアクセスが拒否された
+                self.button.isEnabled = false
+            case .restricted: // この端末で音声認識が出来ない
+                self.button.isEnabled = false
+            case .notDetermined: // 音声認識が許可されていない
+                self.button.isEnabled = false
+            }
+        }
+        setMode(.none)
+    }
+
+    // MARK: - Action
+    @IBAction func tapButton(_ sender: AnyObject) {
+        switch mode {
+        case .none:
+            do {
+                try self.startRecording()
+                setMode(.recording)
+            } catch {
+
+            }
+            break
+        case .recording:
+            stopRecording()
+            startTranslation()
+            setMode(.translation)
+            break
+        case .translation:
+            break
+        }
+    }
+
+    // MARK: - Private
+    fileprivate func startRecording() throws {
+
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object")
+        }
+
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+
+        recognitionRequest.shouldReportPartialResults = true
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            if let result = result {
+                if (self.mode == .recording) {
+                    self.inputTextView.text = result.bestTranscription.formattedString
+                }
+                isFinal = result.isFinal
+            }
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        }
+
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        audioEngine.prepare()
+        try audioEngine.start()
+    }
+
+    fileprivate func stopRecording() {
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+    }
+
+    fileprivate func startTranslation() {
+        translator.conversion(inputTextView.text, complate: { result in
+            self.outputTextView.text = result
+            let utterance = AVSpeechUtterance(string: result)
+            utterance.voice = AVSpeechSynthesisVoice(language: "en")
+            self.talker.speak(utterance)
+
+
+
+            self.setMode(.none)
+        })
+    }
+
+    func setMode(_ mode:Mode){
+        self.mode = mode
+        switch mode {
+        case .none:
+            button.setTitle("開始", for: .normal)
+            indicator.isHidden = true
+            indicator.stopAnimating()
+        case .recording:
+            inputTextView.text = ""
+            outputTextView.text = ""
+            button.setTitle("翻訳", for: .normal)
+            indicator.isHidden = true
+        case .translation:
+            button.setTitle("", for: .normal)
+            indicator.isHidden = false
+            indicator.startAnimating()
+        }
+
+    }
+
+    // MARK: - SFSpeechRecognizerDelegate
+
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            button.isEnabled = true // 利用可能
+        } else {
+            button.isEnabled = false // 利用不能
+        }
+    }
+
+}
